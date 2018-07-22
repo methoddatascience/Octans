88c59c302ba95f8025d25c237bdce8cad1c8177e
katerinabc <kbohlec@gmail.com>
2018-07-22 13:05:02 +0200

    updated rshiny example

--- rshiny_example/ui.R
+++ rshiny_example/ui.R
@@ -54,9 +54,9 @@ consumerKey = "My Key"
 consumerSecret = "My Secret"
 accessToken = "My Token"
 accessSecret = "My Secret"
-my_oauth <- setup_twitter_oauth(consumer_key='0i2JCz10FhsWrJmgy7iLldVXp',
-                                consumer_secret='nkIgmAQ6at24ydUnm5olrOY64OqJ1fHFwYOVYQ636HvJ1mewSy',
-                                access_token='2220997760-6XDKYaSCv3D0jPJ9XRRG5dhOSzmjwc88llGbUa2',
-                                access_secret='ZIMBaNce0ltCeOooPn9giK3lKPNgmUs78rHHoua09ZoQ8')
+my_oauth <- setup_twitter_oauth(consumer_key='consumerkey',
+                                consumer_secret='consumerSecret',
+                                access_token='My Token',
+                                access_secret='My Secret')
 
 
